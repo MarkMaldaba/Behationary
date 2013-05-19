b516395d3bdcfbfa93af5c080aae835c3dd94b9b
meadsteve <meadsteve@gmail.com>
2013-05-19 11:03:23 +0100

    Added a small bullet PHP api to serve up requests for the behationary data. This gets served as json.

---
+++ bootstrap.php
@@ -0,0 +1,8 @@
+<?php
+include_once 'vendor/Autoload.php';
+
+include_once 'MeadSteve/Behationary/Behationary.php';
+include_once 'MeadSteve/Behationary/IndexedContext.php';
+include_once 'MeadSteve/Behationary/StepPrettyfier.php';
+
+include_once 'Tests/MeadSteve/Behationary/FakeContext.php';
---
+++ www/api.php
@@ -0,0 +1,73 @@
+<?php
+require_once __DIR__ . "/../bootstrap.php";
+$configPath = __DIR__ . "/../config.php";
+if (file_exists($configPath)) {
+    include_once $configPath;
+}
+
+$app = new Bullet\App();
+$request = new Bullet\Request();
+
+// 'steps' subdirectory
+$app->path('steps', function($request) use($app) {
+    $behationary = new \MeadSteve\Behationary\Behationary();
+    // If a get contexts functions exists (should have been defined in the
+    // config file. Then call this as it will return all the contexts that
+    // need testing.
+    if (function_exists('\Behationary\getContexts')) {
+        $contexts = \Behationary\getContexts();
+        $behationary->addContexts($contexts);
+    }
+
+    // GetAll
+    $app->get(function() use ($behationary) {
+        return getJsonReadySteps($behationary->getAllSteps());
+    });
+
+    // Filter
+    $app->path('query', function($request) use($app, $behationary) {
+        $app->get(function() use ($behationary, $request) {
+            $searchTerm = $request->query('term', "");
+            $steps = $behationary->getAllSteps();
+            if ($searchTerm !== "") {
+                $steps = array_flip($steps);
+                $steps = array_filter(
+                    $steps,
+                    getStepFilterer($searchTerm)
+                );
+                return getJsonReadySteps(array_flip($steps));
+            }
+            else {
+                return getJsonReadySteps($steps);
+            }
+        });
+    });
+});
+
+$app->on('Exception',
+    function($request, $response, \Exception $e) use($app) {
+        $response->content(array(
+            'exception' => get_class($e),
+            'message' => $e->getMessage()
+        ));
+    }
+);
+
+echo $app->run($request);
+
+function getStepFilterer($searchTerm) {
+    return function($step) use ($searchTerm) {
+        return (strpos($step, $searchTerm) !== false);
+    };
+}
+
+function getJsonReadySteps($steps) {
+    return array_map('getJsonReadyStep', array_keys($steps), $steps);
+}
+
+function getJsonReadyStep($step, $method) {
+    return array(
+        'step'      => $step,
+        'method'    => $method
+    );
+}
