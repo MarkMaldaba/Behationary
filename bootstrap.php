e602339c86eda71c6e2cd33672f86e5a3575d3b7
Mark Clements <github@kennel17.co.uk>
2015-06-04 11:56:55 +0100

    Tidied up bootstrap.php by adding some comments to break things up and annotate the process better.
    I also moved the inclusion of the main Behationary class so it is grouped with the other project classes.

--- bootstrap.php
+++ bootstrap.php
@@ -1,13 +1,18 @@
 <?php
+
+// Include project autoloader, for our project dependencies.
 include_once 'vendor/autoload.php';
 
-include_once 'MeadSteve/Behationary/Behationary.php';
+// Load config file.
 include_once 'MeadSteve/Behationary/Config.php';
 
 $configPath = __DIR__ . "/config.php";
 \MeadSteve\Behationary\Config::setPath($configPath);
 
+// Load the Behationary classes.  We don't use an autoloader for this currently.
+include_once 'MeadSteve/Behationary/Behationary.php';
 include_once 'MeadSteve/Behationary/IndexedContext.php';
 include_once 'MeadSteve/Behationary/StepPrettyfier.php';
 
+// Load the test classes.
 include_once 'Tests/MeadSteve/Behationary/FakeContext.php';
